e7029cd6f699eccd8c2679c371a67344930db4d8
bryanlauwk <75205195+bryanlauwk@users.noreply.github.com>
2026-06-25 15:48:22 +0800

    Load cube glass styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/classic-glass.css";
+import "./styles/cube-glass.css";
 import "@fontsource/abril-fatface/400.css";
 import "@fontsource/cabin/400.css";
 import "@fontsource/cabin/700.css";
