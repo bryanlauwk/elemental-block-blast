003161b25ee8fc9124528ee72fa58c0267c4db62
bryanlauwk <75205195+bryanlauwk@users.noreply.github.com>
2026-06-25 18:19:23 +0800

    Load Cube playfield anchor override

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/classic-glass.css";
 import "./styles/cube-glass.css";
 import "./styles/overhaul-v3.css";
 import "./styles/performance.css";
+import "./styles/cube-anchor.css";
 import "@fontsource/abril-fatface/400.css";
 import "@fontsource/cabin/400.css";
 import "@fontsource/cabin/700.css";
