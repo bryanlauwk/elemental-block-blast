2f153e1676167d0c43c11944c7c9845a7257448c
bryanlauwk <75205195+bryanlauwk@users.noreply.github.com>
2026-06-25 16:38:34 +0800

    Load v3 UI overhaul styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "./styles/classic-glass.css";
 import "./styles/cube-glass.css";
+import "./styles/overhaul-v3.css";
 import "@fontsource/abril-fatface/400.css";
 import "@fontsource/cabin/400.css";
 import "@fontsource/cabin/700.css";
